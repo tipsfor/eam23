4aad69983c76109161b28ceba3063094bcc31b2b
tipsfor <86106308+tipsfor@users.noreply.github.com>
2024-05-01 23:40:58 +0300

    Add files via upload

---
+++ js/carousel.js
@@ -0,0 +1,451 @@
+new Carousel(
+  document.getElementById("one"),
+  {
+    // Ваши настройки карусели
+    Dots: false,
+    compact: false,
+    idle: false,
+    dragToClose: false,
+    commonCaption: true,
+
+    Images: {
+      zoom: false,
+    },
+    showClass: "f-fadeIn",
+
+    Toolbar: {
+      absolute: false,
+      display: {
+        left: ["close", "thumbs", "fullscreen"],
+        middle: [],
+        right: [],
+      },
+    },
+    Thumbs: {
+      type: "classic",
+      Carousel: {
+        center: function () {
+          return this.contentDim > this.viewportDim;
+        },
+      },
+    },
+  },
+  { Thumbs }
+);
+
+
+new Carousel(
+  document.getElementById("two"),
+  {
+    // Ваши настройки карусели
+    Dots: false,
+    compact: false,
+    idle: false,
+    dragToClose: false,
+    commonCaption: true,
+
+    Images: {
+      zoom: false,
+    },
+    showClass: "f-fadeIn",
+
+    Toolbar: {
+      absolute: false,
+      display: {
+        left: ["close", "thumbs", "fullscreen"],
+        middle: [],
+        right: [],
+      },
+    },
+    Thumbs: {
+      type: "classic",
+      Carousel: {
+        center: function () {
+          return this.contentDim > this.viewportDim;
+        },
+      },
+    },
+  },
+  { Thumbs }
+);
+
+Fancybox.bind('[data-fancybox="one"]', {
+  compact: false,
+  idle: false,
+
+  animated: false,
+  showClass: false,
+  hideClass: false,
+
+  dragToClose: false,
+  contentClick: false,
+
+  Images: {
+    // Disable animation from/to thumbnail on start/close
+    zoom: false,
+  },
+
+  Toolbar: {
+    display: {
+      left: [],
+      middle: ['infobar'],
+      right: ['close'],
+    },
+  },
+
+  Thumbs: {
+    type: 'classic',
+  },
+});
+
+
+Fancybox.bind('[data-fancybox="two"]', {
+  compact: false,
+  idle: false,
+
+  animated: false,
+  showClass: false,
+  hideClass: false,
+
+  dragToClose: false,
+  contentClick: false,
+
+  Images: {
+    // Disable animation from/to thumbnail on start/close
+    zoom: false,
+  },
+
+  Toolbar: {
+    display: {
+      left: [],
+      middle: ['infobar'],
+      right: ['close'],
+    },
+  },
+
+  Thumbs: {
+    type: 'classic',
+  },
+});
+new Carousel(
+  document.getElementById("three"),
+  {
+    // Ваши настройки карусели
+    Dots: false,
+    compact: false,
+    idle: false,
+    dragToClose: false,
+    commonCaption: true,
+
+    Images: {
+      zoom: false,
+    },
+    showClass: "f-fadeIn",
+
+    Toolbar: {
+      absolute: false,
+      display: {
+        left: ["close", "thumbs", "fullscreen"],
+        middle: [],
+        right: [],
+      },
+    },
+    Thumbs: {
+      type: "classic",
+      Carousel: {
+        center: function () {
+          return this.contentDim > this.viewportDim;
+        },
+      },
+    },
+  },
+  { Thumbs }
+);
+
+Fancybox.bind('[data-fancybox="three"]', {
+  compact: false,
+  idle: false,
+
+  animated: false,
+  showClass: false,
+  hideClass: false,
+
+  dragToClose: false,
+  contentClick: false,
+
+  Images: {
+    // Disable animation from/to thumbnail on start/close
+    zoom: false,
+  },
+
+  Toolbar: {
+    display: {
+      left: [],
+      middle: ['infobar'],
+      right: ['close'],
+    },
+  },
+
+  Thumbs: {
+    type: 'classic',
+  },
+});
+
+
+new Carousel(
+  document.getElementById("four"),
+  {
+    // Ваши настройки карусели
+    Dots: false,
+    compact: false,
+    idle: false,
+    dragToClose: false,
+    commonCaption: true,
+
+    Images: {
+      zoom: false,
+    },
+    showClass: "f-fadeIn",
+
+    Toolbar: {
+      absolute: false,
+      display: {
+        left: ["close", "thumbs", "fullscreen"],
+        middle: [],
+        right: [],
+      },
+    },
+    Thumbs: {
+      type: "classic",
+      Carousel: {
+        center: function () {
+          return this.contentDim > this.viewportDim;
+        },
+      },
+    },
+  },
+  { Thumbs }
+);
+
+Fancybox.bind('[data-fancybox="four"]', {
+  compact: false,
+  idle: false,
+
+  animated: false,
+  showClass: false,
+  hideClass: false,
+
+  dragToClose: false,
+  contentClick: false,
+
+  Images: {
+    // Disable animation from/to thumbnail on start/close
+    zoom: false,
+  },
+
+  Toolbar: {
+    display: {
+      left: [],
+      middle: ['infobar'],
+      right: ['close'],
+    },
+  },
+
+  Thumbs: {
+    type: 'classic',
+  },
+});
+
+new Carousel(
+  document.getElementById("five"),
+  {
+    // Ваши настройки карусели
+    Dots: false,
+    compact: false,
+    idle: false,
+    dragToClose: false,
+    commonCaption: true,
+
+    Images: {
+      zoom: false,
+    },
+    showClass: "f-fadeIn",
+
+    Toolbar: {
+      absolute: false,
+      display: {
+        left: ["close", "thumbs", "fullscreen"],
+        middle: [],
+        right: [],
+      },
+    },
+    Thumbs: {
+      type: "classic",
+      Carousel: {
+        center: function () {
+          return this.contentDim > this.viewportDim;
+        },
+      },
+    },
+  },
+  { Thumbs }
+);
+
+Fancybox.bind('[data-fancybox="five"]', {
+  compact: false,
+  idle: false,
+
+  animated: false,
+  showClass: false,
+  hideClass: false,
+
+  dragToClose: false,
+  contentClick: false,
+
+  Images: {
+    // Disable animation from/to thumbnail on start/close
+    zoom: false,
+  },
+
+  Toolbar: {
+    display: {
+      left: [],
+      middle: ['infobar'],
+      right: ['close'],
+    },
+  },
+
+  Thumbs: {
+    type: 'classic',
+  },
+});
+
+
+new Carousel(
+  document.getElementById("six"),
+  {
+    // Ваши настройки карусели
+    Dots: false,
+    compact: false,
+    idle: false,
+    dragToClose: false,
+    commonCaption: true,
+
+    Images: {
+      zoom: false,
+    },
+    showClass: "f-fadeIn",
+
+    Toolbar: {
+      absolute: false,
+      display: {
+        left: ["close", "thumbs", "fullscreen"],
+        middle: [],
+        right: [],
+      },
+    },
+    Thumbs: {
+      type: "classic",
+      Carousel: {
+        center: function () {
+          return this.contentDim > this.viewportDim;
+        },
+      },
+    },
+  },
+  { Thumbs }
+);
+
+Fancybox.bind('[data-fancybox="six"]', {
+  compact: false,
+  idle: false,
+
+  animated: false,
+  showClass: false,
+  hideClass: false,
+
+  dragToClose: false,
+  contentClick: false,
+
+  Images: {
+    // Disable animation from/to thumbnail on start/close
+    zoom: false,
+  },
+
+  Toolbar: {
+    display: {
+      left: [],
+      middle: ['infobar'],
+      right: ['close'],
+    },
+  },
+
+  Thumbs: {
+    type: 'classic',
+  },
+});
+
+
+new Carousel(
+  document.getElementById("seven"),
+  {
+    // Ваши настройки карусели
+    Dots: false,
+    compact: false,
+    idle: false,
+    dragToClose: false,
+    commonCaption: true,
+
+    Images: {
+      zoom: false,
+    },
+    showClass: "f-fadeIn",
+
+    Toolbar: {
+      absolute: false,
+      display: {
+        left: ["close", "thumbs", "fullscreen"],
+        middle: [],
+        right: [],
+      },
+    },
+    Thumbs: {
+      type: "classic",
+      Carousel: {
+        center: function () {
+          return this.contentDim > this.viewportDim;
+        },
+      },
+    },
+  },
+  { Thumbs }
+);
+
+Fancybox.bind('[data-fancybox="seven"]', {
+  compact: false,
+  idle: false,
+
+  animated: false,
+  showClass: false,
+  hideClass: false,
+
+  dragToClose: false,
+  contentClick: false,
+
+  Images: {
+    // Disable animation from/to thumbnail on start/close
+    zoom: false,
+  },
+
+  Toolbar: {
+    display: {
+      left: [],
+      middle: ['infobar'],
+      right: ['close'],
+    },
+  },
+
+  Thumbs: {
+    type: 'classic',
+  },
+});
